70acd4c1181ce491da6359e942ae072062084208
Luto228 <megapupsik909@gmail.com>
2026-05-07 23:23:42 +0400

    feat: add entry point and command dispatcher

--- main.py
+++ main.py
@@ -1,30 +1,20 @@
 #!/usr/bin/env python3
 import sys
-import os
 
 def main():
-    length = len(sys.argv)
-    if length < 2 or length > 6:
-        print("\n--- Project Auto-Starter ---")
-        print("Usage: doit <repo_name> [public on/off] [readme on/off] [license/off] [gitignore/off]")
-        print("\nSetup: alias doit='/your_path/main.py' in .bashrc")
-        sys.exit(1)
-    name = sys.argv[1]
-    command = f'gh repo create {name}'
-    if length >= 3:
-        if sys.argv[2] == 'on':
-            command += ' --public'
-        elif sys.argv[2] == 'off':
-            command += ' --private'
-    if length >= 4:
-        if sys.argv[3] == 'on':
-            command += ' --add-readme'
-    if length >= 5:
-        if sys.argv[4] != 'off':
-            command += f' --license {sys.argv[4].lower()}'
-    if length == 6:
-        if sys.argv[5] != 'off':
-            command += f' --gitignore {sys.argv[5]}'
-    os.system(command)
+    if len(sys.argv) < 2:
+        print("Usage: github <command> [args]")
+        print("Commands: doit, find")
+        return
+
+    if sys.argv[1] == "doit":
+        import doit
+        doit.main(sys.argv[2:])
+    elif sys.argv[1] == "find":
+        import find
+        find.main(sys.argv[2:])
+    else:
+        print(f"Unknown command: {sys.argv[1]}")
+
 if __name__ == "__main__":
     main()
